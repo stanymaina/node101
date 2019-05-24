4e177ae92babff9b72c88e791466ea52a8f73668
root <root@localhost.localdomain>
2019-05-25 00:32:36 +0100

    Added node parts

--- server/routes/index.js
+++ server/routes/index.js
@@ -16,11 +16,12 @@ export default (app) => {
     // Book Routes
     const BOOK_ROUTE = '/books'
     app.get(API_ROOT+BOOK_ROUTE, Books.index); // API route for getting all books
-    app.get(API_ROOT+BOOK_ROUTE+'create', Books.create); // API route for creating a book
-    app.get(API_ROOT+BOOK_ROUTE+':bookId', Books.show); // API route for showing a specific book
-    app.post(API_ROOT+BOOK_ROUTE+':bookId', Books.store); // API route for user to edit a book
-    app.get(API_ROOT+BOOK_ROUTE+':bookId/edit', Books.edit); // API route for edit a book
-    app.put(API_ROOT+BOOK_ROUTE+':bookId', Books.update); // API route for user to edit a book
-    app.delete(API_ROOT+BOOK_ROUTE+':bookId', Books.delete); // API route for user to delete a book
+    app.get(API_ROOT+BOOK_ROUTE+'/:bookId', Books.show); // API route for showing a specific book
+    app.post(API_ROOT+BOOK_ROUTE+'/:bookId', Books.store); // API route for user to edit a book
+    app.put(API_ROOT+BOOK_ROUTE+'/:bookId', Books.update); // API route for user to edit a book
+    app.delete(API_ROOT+BOOK_ROUTE+'/:bookId', Books.delete); // API route for user to delete a book
+
+    app.get(API_ROOT+BOOK_ROUTE+'/create', Books.create); // API route for creating a book
+    app.get(API_ROOT+BOOK_ROUTE+'/:bookId/edit', Books.edit); // API route for edit a book
 
 };
