d2b211540af357e9d6f57a6686846ee57440db36
root <root@localhost.localdomain>
2019-05-27 10:02:02 +0100

    Added social login

---
+++ server/config/auth.js
@@ -0,0 +1,38 @@
+  
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth': {
+        'clientID': '', // your App ID
+        'clientSecret': '', // your App Secret
+        'callbackURL': 'http://localhost:3000/auth/facebook/callback',
+        'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
+
+    },
+
+    'googleAuth': {
+        'clientID': '', // your Client ID
+        'clientSecret': '', // your Client Secret
+        'callbackURL': 'http://localhost:3000/auth/google/callback'
+    },
+
+    // 'instagramAuth': {
+    //     'clientID': '',
+    //     'clientSecret': ' ',
+    //     'callbackURL': 'http://127.0.0.1:3000/auth/instagram/callback'
+    // },
+
+    'githubAuth': {
+        'clientID': '',
+        'clientSecret': '',
+        'callbackURL': "http://localhost:3000/auth/github/callback"
+    },
+
+    'twitterAuth': {
+        'consumerKey': '',
+        'consumerSecret': '',
+        'callbackURL': "http://localhost:3000/auth/twitter/callback"
+    },
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -33,4 +33,33 @@ export default (app) => {
     app.get(API_ROOT+BOOK_ROUTE+'/create/', Books.create); // API route for creating a book
     app.get(API_ROOT+BOOK_ROUTE+'/:bookId/edit', Books.edit); // API route for edit a book
 
+    app.get('/login/facebook', FacebookRoutes.authenticate() );
+    app.get( '/callback/facebook', FacebookRoutes.callback(), redirectSocialUser );
+    /**
+    * @api {POST} /auth/login/google Social Login
+    * @apiName google
+    * @apiGroup Auth
+    * @apiSuccess {String} code HTTP status code from API.
+    * @apiSuccess {String} message Message from API.
+    */
+    app.get('/login/google', GoogleRoutes.authenticate() );
+    app.get( '/callback/google', GoogleRoutes.callback(), redirectSocialUser );
+    /**
+    * @api {POST} /auth/login/twitter Social Login
+    * @apiName twitter
+    * @apiGroup Auth
+    * @apiSuccess {String} code HTTP status code from API.
+    * @apiSuccess {String} message Message from API.
+    */
+    app.get('/login/twitter', TwitterRoutes.authenticate() );
+    app.get( '/callback/twitter', TwitterRoutes.callback(), redirectSocialUser );
+    /**
+    * @api {POST} /auth/login/instagram Social Login
+    * @apiName instagram
+    * @apiGroup Auth
+    * @apiSuccess {String} code HTTP status code from API.
+    * @apiSuccess {String} message Message from API.
+    */
+    app.get('/login/instagram', InstagramRoutes.authenticate() );
+    app.get( '/callback/instagram', InstagramRoutes.callback(), redirectSocialUser );
 };
